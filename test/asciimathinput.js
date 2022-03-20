cb663840198671e19ae9db9ea6b9f771b1f21be6
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-20 12:49:36 +0800

    Update asciimathinput.js

--- test/asciimathinput.js
+++ test/asciimathinput.js
@@ -1,1 +1,5 @@
-export const asciimathinput = "sum_(i=1)^n i^3=((n(n+1))/2)^2";
+export const asciimathinput =`pi=lim_(m->oo)(sum_(n=0)^(m) 
+(
+((-1)^n)/(2^6*1024^n)*((-2^5)/(4n+1)+(-1)/(4n+3)+(2^8)/(10n+1)+(-2^6)/(10n+3)+(-2^2)/(10n+5)+(-2^2)/(10n+7)+(1)/(10n+9))
+)
+)`
