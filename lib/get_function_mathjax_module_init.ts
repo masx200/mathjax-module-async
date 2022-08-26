7b7d4ee607bf7570e58ba10cf5455411e3484e36
masx200 <34191203+masx200@users.noreply.github.com>
2022-08-26 12:18:31 +0800

    优化

--- lib/get_function_mathjax_module_init.ts
+++ lib/get_function_mathjax_module_init.ts
@@ -4,15 +4,20 @@ export async function get_function_mathjax_module_init(
 ): Promise<string> {
     const funbody = args[args.length - 1];
     const funargs = args.slice(0, args.length - 1);
+    const code = (await minify(funbody, terserOptions)).code;
+    if (!code) throw Error('accident')
     /* 对于不支持严格模式的代码的处理,只能使用new Function */
     // const scriptbody = `export default function(${funargs.join(
-    //     "\n,\n"
-    // )}){\n${funbody}\n}`;
+    //     "\n,\n",
+    // )
+    //     }){\n${code}\n}`;
     const scriptbody = `export default new Function(${funargs
         .map((a) => JSON.stringify(a))
-        .join(",")},${JSON.stringify(
-        (await minify(funbody, terserOptions)).code
-    )});`;
+        .join(",")
+        },${JSON.stringify(
+            code//.replace('t.callee.SUPER', "console.log(t),t.callee.SUPER"),
+        )
+        });`;
     return scriptbody;
 }
 const terserOptions = {
