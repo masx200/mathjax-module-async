93fd72c8e17b01153542f10c74849c7aa27250a3
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-05 22:22:33 +0800

    versionorigin

--- lib/mathjaxurl.ts
+++ lib/mathjaxurl.ts
@@ -1,4 +1,4 @@
-import { version } from "./version.js";
+import { version, versionorigin } from "./version.js";
 
 export const mathjaxbase = `https://denopkg.com/masx200/MathJax@${version}/es5`;
-export const mathjaxurl = `https://unpkg.com/mathjax@${version}/es5`;
+export const mathjaxurl = `https://unpkg.com/mathjax@${versionorigin}/es5`;
--- lib/version.ts
+++ lib/version.ts
@@ -1,2 +1,2 @@
 export const version = "3.2.2-delete-arguments.callee";
-// export const version = "3.2.2";
+export const versionorigin = "3.2.2";
