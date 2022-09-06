2737abf8096bc6afdc967d455ba41ebf5bed5762
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-06 20:53:59 +0800

    Update browser.test.ts

--- test/browser.test.ts
+++ test/browser.test.ts
@@ -3,7 +3,14 @@ import path from "path";
 //@ts-ignore
 import createBrowserless from "browserless";
 import { main } from "@masx200/serve-cli";
-
+import { parseHTML } from "linkedom";
+import assert from "assert";
+import { latexoutput } from "./latex-output";
+import { mathmloutput } from "./mathml-output";
+import { asciimathoutput } from "./asciimath-output";
+function JSDOM(html: any) {
+    return parseHTML(html);
+}
 test(
     "browser",
     async () => {
@@ -14,10 +21,22 @@ test(
         const browserless = createBrowserless({});
         const ctx = await browserless.createContext();
         const html = await ctx.html("http://localhost:4000");
-        console.log(html);
+        // console.log(html);
         server.close();
 
         await browserless.close();
+        const { document } = JSDOM(html);
+        // console.log(document);
+
+        Array.from(document.querySelectorAll("#latex"))
+            .map((a) => a.innerHTML)
+            .forEach((t) => assert.equal(t, latexoutput));
+        Array.from(document.querySelectorAll("#asciimath"))
+            .map((a) => a.innerHTML)
+            .forEach((t) => assert.equal(t, asciimathoutput));
+        Array.from(document.querySelectorAll("#mathml"))
+            .map((a) => a.innerHTML)
+            .forEach((t) => assert.equal(t, mathmloutput));
     },
     10 * 1000
 );
