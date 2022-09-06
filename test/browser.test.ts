4a7eef61c3b96d21054df1cfa2837234c99a27e0
masx200 <34191203+masx200@users.noreply.github.com>
2022-09-06 20:21:28 +0800

    测试

--- test/browser.test.ts
+++ test/browser.test.ts
@@ -1,2 +1,20 @@
 import { test } from "vitest";
-test("browser", async () => {});
+import path from "path";
+//@ts-ignore
+import browserlessFactory from "browserless";
+import { main } from "@masx200/serve-cli";
+import exitHook from "exit-hook";
+test("browser", async () => {
+    const server = main({ port: 4000, path: path.resolve(__dirname, "dist") });
+    const { createContext } = browserlessFactory({});
+
+    // Now every time you call `createContext`
+    // it will be create a browser context.
+    const browserless = await createContext();
+
+    const html = await browserless.html("http://localhost:4000");
+    console.log(html);
+    server.close();
+
+    exitHook(browserlessFactory.close());
+});
